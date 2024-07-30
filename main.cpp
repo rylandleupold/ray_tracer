f76bb027fcb0939751df9f060060730bbbfc936e
rylandleupold <56704182+rylandleupold@users.noreply.github.com>
2024-07-29 21:57:57 -0700

    Implement texture coordinate mapping for images

--- main.cpp
+++ main.cpp
@@ -99,14 +99,37 @@ void checkered_spheres() {
     cam.render(world);
 }
 
+void earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(0,0,12);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
 int main(int, char**){
     const auto start_time = std::chrono::high_resolution_clock::now();
 
-    int scene = 2;
+    int scene = 3;
     
     switch (scene) {
         case 1: bouncing_spheres();  break;
         case 2: checkered_spheres(); break;
+        case 3: earth();             break;
     }
 
     const auto end_time = std::chrono::high_resolution_clock::now();
--- sphere.h
+++ sphere.h
@@ -46,8 +46,10 @@ class sphere : public hittable {
 
             rec.t = root;
             rec.p = r.at(rec.t);
+            double u, v;
             vec3 outward_normal = (rec.p - center) / radius; // Special way to normalize a vector for spheres only
             rec.set_face_normal(r, outward_normal);
+            get_sphere_uv(outward_normal, rec.u, rec.v);
             rec.mat = mat;
             return true;
         };
@@ -66,6 +68,20 @@ class sphere : public hittable {
             // Linearly interpolate from center1 to center2 according to time, where t=0 yields
             // center1, and t=1 yields center2.
             return center1 + time * center_vec;
+        }
+
+        static void get_sphere_uv(const point3& p, double& u, double& v) {
+            // p: a given point on the sphere of radius one, centered at the origin.
+            // u: returned value [0,1] of angle around the Y axis from X=-1.
+            // v: returned value [0,1] of angle from Y=-1 to Y=+1.
+            //     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00 0.50>
+            //     <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields <0.50 0.00>
+            //     <0 0 1> yields <0.25 0.50>       < 0  0 -1> yields <0.75 0.50>
+            auto theta = std::acos(-p.y());
+            auto phi = std::atan2(-p.z(), p.x()) + pi;
+
+            u = phi / (2*pi);
+            v = theta / pi;
         }
 };
 
