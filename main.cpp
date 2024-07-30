fc83fc275c1c01954dd9fccbe928f6536b9d3aaa
rylandleupold <56704182+rylandleupold@users.noreply.github.com>
2024-07-30 00:21:05 -0700

    quadrilaterals

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 #include "camera.h"
 #include "bvh.h"
 #include "texture.h"
+#include "quad.h"
 
-double elapsed_seconds(const std::chrono::steady_clock::time_point& start_time, const std::chrono::steady_clock::time_point& end_time) {
+double elapsed_seconds(const std::chrono::steady_clock::time_point& start_time, const std::chrono::steady_clock::time_point& end_time, int decimal_places) {
     double elapsed_millis = std::chrono::duration<double, std::milli>(end_time - start_time).count(); 
-    return elapsed_millis / 1000.0;
+    double decimal_scale = std::pow(10.0, decimal_places);
+    return std::ceil(elapsed_millis / 1000.0 * decimal_scale) / decimal_scale;
 }
 
 void bouncing_spheres() {
@@ -145,18 +147,53 @@ void perlin_spheres() {
     cam.render(world);
 }
 
+void quads() {
+    hittable_list world;
+
+    // Materials
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 80;
+    cam.lookfrom = point3(0,0,9);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main(int, char**){
     const auto start_time = std::chrono::high_resolution_clock::now();
 
-    int scene = 4;
+    int scene = 5;
     
     switch (scene) {
         case 1: bouncing_spheres();  break;
         case 2: checkered_spheres(); break;
         case 3: earth();             break;
         case 4: perlin_spheres();    break;
+        case 5: quads();             break;
     }
 
     const auto end_time = std::chrono::high_resolution_clock::now();
-    std::cout << "Total Time: " << elapsed_seconds(start_time, end_time) << "s\n";
+    std::cout << "Total Time: " << elapsed_seconds(start_time, end_time, 3) << "s\n";
 }
